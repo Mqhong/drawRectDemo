a80190be3ce6be118a527c9fbd713350071ed3b4
梦梦 <mm@macbook-pro.local>
2015-10-23 01:42:12 +0800

    Initial Commit

---
+++ 下载进度条/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  下载进度条
+//
+//  Created by mm on 15/10/23.
+//  Copyright © 2015年 mm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 下载进度条/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  下载进度条
+//
+//  Created by mm on 15/10/23.
+//  Copyright © 2015年 mm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
